9b6adbd020fb0b2377d4750844234421362ee16d
rithik <rithikrock812@gmail.com>
2026-04-28 12:10:37 +0530

    code changes

--- app/src/main/java/com/example/myapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/myapp/di/NetworkModule.kt
@@ -6,12 +6,14 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
     @Provides
+    @Singleton
     fun provideRetrofit(): Retrofit =
         Retrofit.Builder()
             .baseUrl("https://api.freeapi.app/")
@@ -19,6 +21,7 @@ object NetworkModule {
             .build()
 
     @Provides
+    @Singleton
     fun provideApi(retrofit: Retrofit): ProductApi =
         retrofit.create(ProductApi::class.java)
 }
